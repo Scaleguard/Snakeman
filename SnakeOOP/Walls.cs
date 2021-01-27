e22cf6e2ad83b03f2e7341aafb94e66a49493bc2
unknown <ampercustoms@gmaile.com>
2021-01-27 23:42:58 +0200

    yes

---
+++ SnakeOOP/Walls.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontalLine top = new HorizontalLine(0, 80, 0, '#');
+            VerticalLine left = new VerticalLine(0, 25, 0, '#');
+            HorizontalLine bottom = new HorizontalLine(0, 80, 25, '$');
+            VerticalLine right = new VerticalLine(0, 25, 80, '$');
+
+            VerticalLine obstacle = new VerticalLine(10, 13, 50, '%');
+            HorizontalLine theTwos = new HorizontalLine(10, 20, 10, '2');
+            VerticalLine theGoodFives = new VerticalLine(5, 7, 25, '5');
+            HorizontalLine theDecentFours = new HorizontalLine(5, 23, 23, '4');
+            wallList.Add(top);
+            wallList.Add(left);
+            wallList.Add(bottom);
+            wallList.Add(right);
+            wallList.Add(obstacle);
+            wallList.Add(theTwos);
+            wallList.Add(theGoodFives);
+            wallList.Add(theDecentFours);
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+
+        public bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
